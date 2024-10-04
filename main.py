b4d539fbca5333df2b46cddf390b1edd4b9772b5
vselected <dominik.sarleja@gmail.com>
2024-10-04 20:12:50 +0200

    Print formatting with Strings nad float

--- main.py
+++ main.py
@@ -2,7 +2,21 @@
 if __name__ == '__main__':
     print('PyCharm')
 
+    #Print formatting with Strings
     print("This is a string {}".format("Inserted"))
     print("The {2} {1} {0}".format("fox", "brown", "quick"))
+    print("The {q} {b} {f}".format(f = "fox", b = "brown", q = "quick"))
+
+    #Print formating with float
+    result = 0.4231421432
+    print("The result was {}".format(result))
+    print("The result was {r}".format(r = result))
+
+    #Float formatting "{value:width.precision f}"
+    print("The result was {r:1.5f}".format(r=result))
+
+    name = "John"
+    age = 3
+    print(f"My name is {name} and I have {age} years")
 
 
