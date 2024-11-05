6632214ecfcf5c54a8e5866deedc7ffdc06e5e59
vselected <dominik.sarleja@gmail.com>
2024-11-05 10:33:29 +0100

    Learned about if, elif and else not For Loops

---
+++ main.py
@@ -0,0 +1,32 @@
+# if, elif and else statements
+if True:
+    print("It's True")
+
+if 3>2:
+    print("It's True")
+
+hungry = False
+if hungry:
+    print("Feed me")
+else:
+    print("I'm not hungry")
+
+location = "Somehting"
+if location == "Auto Shop":
+    print("Cars are cool")
+elif location == "Bank":
+    print("Money is cool")
+elif location == "Store":
+    print("Welcome to the store")
+else:
+    print("I don't know much")
+
+name = "Sam"
+if name == "Frankie":
+    print("Hello Frankie")
+elif name == "Sammy":
+    print("Hello Sammy")
+else:
+    print("Hello "+name+", pleasure to meet you")
+
+# For Loops
